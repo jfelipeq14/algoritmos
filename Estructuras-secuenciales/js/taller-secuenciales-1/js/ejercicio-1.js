90bf3cd9df08a79f327cf88b872fbbbb7c0a8a6e
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-10-23 16:53:31 -0500

    ejercicios de taller 1 de secuenciales: revision de ejercicios

---
+++ Estructuras-secuenciales/js/taller-secuenciales-1/js/ejercicio-1.js
@@ -0,0 +1,17 @@
+let e1, e2, e3, e4, e5, e6, e7, e8, e9, e10
+
+e1 = parseInt(prompt("Ingrese la edad 1: "));
+e2 = parseInt(prompt("Ingrese la edad 2: "));
+e3 = parseInt(prompt("Ingrese la edad 3: "));
+e4 = parseInt(prompt("Ingrese la edad 4: "));
+e5 = parseInt(prompt("Ingrese la edad 5: "));
+e6 = parseInt(prompt("Ingrese la edad 6: "));
+e7 = parseInt(prompt("Ingrese la edad 7: "));
+e8 = parseInt(prompt("Ingrese la edad 8: "));
+e9 = parseInt(prompt("Ingrese la edad 9: "));
+e10 = parseInt(prompt("Ingrese la edad 10: "));
+
+let suma = e1 + e2 + e3 + e4 + e5 + e6 + e7 + e8 + e9 + e10;
+let promedio = suma / 10; //5.5
+
+console.log(`El promedio de las 10 edades es: ${promedio}`);
