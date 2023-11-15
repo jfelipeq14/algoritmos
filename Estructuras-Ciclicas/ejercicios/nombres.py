822a1197ffd91a3c16f7f4cd5e2480a63f9f6ee0
jfelipeq14 <146882792+jfelipeq14@users.noreply.github.com>
2023-11-15 07:10:09 -0500

    nombres con y sin vectores. nombre y edad 4 veces

--- Estructuras-Ciclicas/ejercicios/nombres.py
+++ Estructuras-Ciclicas/ejercicios/nombres.py
@@ -1,6 +1,12 @@
+# vectores
 nombres=[]
 for i in range(5):
     nombre = input(f"Digite el nombre {i+1}: ")
     nombres.append(nombre)
 
 print(nombres)
+
+#sin vectores
+for i in range(5):
+    nombre=input(f"Digite el nombre del aprendiz {i+1}: ")
+    print(f"El nombre del aprendiz es {i+1}")
---
+++ Estructuras-Ciclicas/ejercicios/nombreyedad.py
@@ -0,0 +1,6 @@
+for i in range(4):
+    nombre = input(f"Digite el nombre de la persona #{i+1}: ")
+    edad = int(input(f"Digite la edad de la persona #{i+1}: "))
+
+    print("--")
+    print(f"El nombre de la persona es {nombre} y su edad es {edad}")
