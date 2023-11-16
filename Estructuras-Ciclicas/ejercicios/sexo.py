ffffd408a536a0d8adee314aa6d2d698f793bf0e
jfelipeq14 <146882792+jfelipeq14@users.noreply.github.com>
2023-11-16 06:28:55 -0500

    sexo de 10 empleados

---
+++ Estructuras-Ciclicas/ejercicios/sexo.py
@@ -0,0 +1,13 @@
+# elabore un algoritmo que lea el sexo de 10 empleados y muestre cuantos son hombres y cuantos son mujeres.
+hombres=0
+mujeres=0
+
+for i in range(10):
+    sexo = input(f"Digite el sexo de la persona {i+1}: ")
+    if sexo == "hombre":
+        hombres = hombres + 1
+    elif sexo == "mujer":
+        mujeres = mujeres + 1
+
+print(f"La cantidad de hombres es {hombres}")
+print(f"La cantidad de mujeres es {mujeres}")
