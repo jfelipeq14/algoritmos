ba96a30bccebcbcf636b0ab7bdad369252723873
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-11-22 14:40:36 -0500

    enunciados para ejercicios

---
+++ Estructuras-Ciclicas/anidados/2.py
@@ -0,0 +1,4 @@
+# 2. El supermercado “Todo hay” lo contrata a Ud. joven aprendiz para realizar un
+# algoritmo que calcule las ventas anuales, ventas semestrales y mensuales de la
+# empresa. Cada mes se generan 30 facturas y cada factura (cantidad * precio
+# unitario) tiene 5 artículos.
