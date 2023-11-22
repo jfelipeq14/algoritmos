aa899a3b154992a366d2f4c21a61dbd6c8b7fad6
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-11-22 18:02:39 -0500

    ejercicio 4 incompleto

--- Estructuras-Ciclicas/anidados/4.py
+++ Estructuras-Ciclicas/anidados/4.py
@@ -1,3 +1,8 @@
 # 4. En su proyecto formativo se pide que calcule las comisiones de cada uno de los
 # vendedores de la empresa y el total de comisiones. Se conoce que la empresa
 # tiene 6 vendedores y cada vendedor trae las facturas(3) del mes que vendió.
+
+for vendedores in range(1, 7):
+    for factura in range(1, 4):
+        valorFactura = int(input(f"Digite el valor de la factura {factura}"))
+        
