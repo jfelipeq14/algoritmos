f826fff11a05feebae31c45f14ef85d048a92d0c
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-11-22 18:15:35 -0500

    ejercicio 4 copilot

--- Estructuras-Ciclicas/anidados/4.py
+++ Estructuras-Ciclicas/anidados/4.py
@@ -2,7 +2,20 @@
 # vendedores de la empresa y el total de comisiones. Se conoce que la empresa
 # tiene 6 vendedores y cada vendedor trae las facturas(3) del mes que vendió.
 
-for vendedores in range(1, 7):
+# Variables a usar
+totalComisiones = 0
+
+# Ciclo para recorrer los vendedores
+for vendedor in range(1, 7):
+    # Variables a usar
+    totalComisionesVendedor = 0
+
+    # Ciclo para recorrer las facturas
     for factura in range(1, 4):
-        valorFactura = int(input(f"Digite el valor de la factura {factura}"))
-        
+        # Variables a usar
+        valorFactura = float(input(f"Digite el valor de la factura {factura}: "))
+        comision = valorFactura*0.10
+        totalComisionesVendedor += comision
+        totalComisiones += comision
+    print(f"Total de comisiones del vendedor {vendedor}: {totalComisionesVendedor}")
+print(f"Total de comisiones: {totalComisiones}")
