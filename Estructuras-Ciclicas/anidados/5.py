7b54ef2e18273201fda41efa7ad8f7a85ceec319
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-11-22 18:10:15 -0500

    ejercicio 5 completo (no anidado)

--- Estructuras-Ciclicas/anidados/5.py
+++ Estructuras-Ciclicas/anidados/5.py
@@ -1,3 +1,13 @@
 # 5. En su empresa le solicitan que desarrolle un algoritmo que calcule el iva que
 # pagaron durante el año sabiendo que se paga cada dos meses. (iva =valorfactura *
 # 19%.). 
+
+totaliva = 0
+totalfactura = 0
+
+for mes in range(6):
+    valorfactura = float(input("Ingrese el valor de la factura: "))
+    iva = valorfactura * 0.19
+    totaliva = totaliva + iva
+    totalfactura = totalfactura + valorfactura
+print(f"El total de iva pagado es: {totaliva}")
