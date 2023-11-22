736ace25a49d2e56ff64247c4513972880ce84ac
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-11-22 18:13:23 -0500

    ejercicio 2 copilot

--- Estructuras-Ciclicas/anidados/2.py
+++ Estructuras-Ciclicas/anidados/2.py
@@ -3,19 +3,37 @@
 # empresa. Cada mes se generan 30 facturas y cada factura (cantidad * precio
 # unitario) tiene 5 artículos.
 
-x=0
+# Variables a usar
+totalVentasAnuales = 0
+totalVentasSemestrales = 0
+totalVentasMensuales = 0
 
-anuales = 0
-semestrales = 0
-mensuales = 0
+# Ciclo para recorrer los meses
+for mes in range(1, 13):
+    # Variables a usar
+    totalVentasMes = 0
+    totalVentasSemestre = 0
+    totalVentasAnual = 0
 
-for anuales in range(x):
-    for semestrales in range(1,3):
-        for mes in range(1, 7):
-            for facturas in range(1, 31):
-                for productos in range(1, 6):
-                    nombre = input(f"Digite el nombre del producto #{productos}: ")
-                    cantidad = int(input(f"Digite la cantidad de {nombre}: "))
-                    precio = int(input(f"Digite el precio de {nombre}: "))
-                    totalProducto = cantidad * precio
-                    
+    # Ciclo para recorrer las facturas
+    for factura in range(1, 31):
+        # Variables a usar
+        totalFactura = 0
+
+        # Ciclo para recorrer los articulos
+        for articulo in range(1, 6):
+            # Variables a usar
+            nombreArticulo = input(f"Digite el nombre del articulo {articulo}: ")
+            precioArticulo = int(input(f"Digite el precio de {nombreArticulo}: "))
+            cantidadArticulo = int(input(f"Digite la cantidad de {nombreArticulo}: "))
+            totalArticulo = precioArticulo*cantidadArticulo
+            totalFactura += totalArticulo
+        totalVentasMes += totalFactura
+        totalVentasSemestre += totalFactura
+        totalVentasAnual += totalFactura
+    totalVentasMensuales += totalVentasMes
+    totalVentasSemestrales += totalVentasSemestre
+    totalVentasAnuales += totalVentasAnual
+    print(f"Total de ventas del mes {mes}: {totalVentasMes}")
+    print(f"Total de ventas del semestre {mes}: {totalVentasSemestre}")
+    print(f"Total de ventas del año {mes}: {totalVentasAnual}")
