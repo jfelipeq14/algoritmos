98c7553094c36a27c34a695e3184cf4ed2639fdf
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-11-22 17:34:23 -0500

    ejercicio 2 incompleto

--- Estructuras-Ciclicas/anidados/2.py
+++ Estructuras-Ciclicas/anidados/2.py
@@ -2,3 +2,20 @@
 # algoritmo que calcule las ventas anuales, ventas semestrales y mensuales de la
 # empresa. Cada mes se generan 30 facturas y cada factura (cantidad * precio
 # unitario) tiene 5 artículos.
+
+x=0
+
+anuales = 0
+semestrales = 0
+mensuales = 0
+
+for anuales in range(x):
+    for semestrales in range(1,3):
+        for mes in range(1, 7):
+            for facturas in range(1, 31):
+                for productos in range(1, 6):
+                    nombre = input(f"Digite el nombre del producto #{productos}: ")
+                    cantidad = int(input(f"Digite la cantidad de {nombre}: "))
+                    precio = int(input(f"Digite el precio de {nombre}: "))
+                    totalProducto = cantidad * precio
+                    
