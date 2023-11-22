d1029431622a7a2777849ffa417afb3ea080a163
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-11-22 18:14:48 -0500

    ejercicio 3 copilot

--- Estructuras-Ciclicas/anidados/3.py
+++ Estructuras-Ciclicas/anidados/3.py
@@ -4,37 +4,59 @@
 # 32 regionales y cada regional tiene 8 centros de formación (cada centro tiene 10
 # empleados).
 
-#Acumuladores
+# Variables a usar
 totalSolteros = 0
 totalCasados = 0
+totalSeparados = 0
 totalViudos = 0
 totalUnionLibre = 0
-totalSeparados = 0
 
-solteros = 0
-casados = 0
-viudos = 0
-unionLibre = 0
-separados = 0
-
+# Ciclo para recorrer las regionales
 for regional in range(1, 33):
-    for centroDeFormacion in range(1,9):
+    # Variables a usar
+    totalSolterosRegional = 0
+    totalCasadosRegional = 0
+    totalSeparadosRegional = 0
+    totalViudosRegional = 0
+    totalUnionLibreRegional = 0
+
+    # Ciclo para recorrer los centros de formacion
+    for centro in range(1, 9):
+        # Variables a usar
+        totalSolterosCentro = 0
+        totalCasadosCentro = 0
+        totalSeparadosCentro = 0
+        totalViudosCentro = 0
+        totalUnionLibreCentro = 0
+
+        # Ciclo para recorrer los empleados
         for empleado in range(1, 11):
-            estadoCivil = input(f"Digite el estado civil del empleado {empleado}").lower()
+            # Variables a usar
+            estadoCivil = input(f"Digite el estado civil del empleado {empleado}: ").lower()
 
             if estadoCivil == "soltero":
-                solteros += 1
+                totalSolteros += 1
+                totalSolterosRegional += 1
+                totalSolterosCentro += 1
             elif estadoCivil == "casado":
-                casados += 1
-            elif estadoCivil == "viudo":
-                viudos += 1
-            elif estadoCivil == "union libre":
-                unionLibre += 1
+                totalCasados += 1
+                totalCasadosRegional += 1
+                totalCasadosCentro += 1
             elif estadoCivil == "separado":
-                separados += 1
-
-            totalSolteros += solteros
-            totalCasados += casados
-            totalViudos += viudos
-            totalUnionLibre += unionLibre
-            totalSeparados += separados
+                totalSeparados += 1
+                totalSeparadosRegional += 1
+                totalSeparadosCentro += 1
+            elif estadoCivil == "viudo":
+                totalViudos += 1
+                totalViudosRegional += 1
+                totalViudosCentro += 1
+            elif estadoCivil == "union libre":
+                totalUnionLibre += 1
+                totalUnionLibreRegional += 1
+                totalUnionLibreCentro += 1
+        print(f"Total de solteros del centro {centro}: {totalSolterosCentro}")
+        print(f"Total de casados del centro {centro}: {totalCasadosCentro}")
+        print(f"Total de separados del centro {centro}: {totalSeparadosCentro}")
+        print(f"Total de viudos del centro {centro}: {totalViudosCentro}")
+        print(f"Total de union libre del centro {centro}: {totalUnionLibreCentro}")
+    print(f"Total de solteros de la regional {regional}: {totalSolterosRegional}")
