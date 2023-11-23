8013baa5a23f9218fe8e1e07a7ed3c0e02d03fa3
Juan Felipe Quintero Gutierrez <146882792+jfelipeq14@users.noreply.github.com>
2023-11-23 07:42:39 -0500

    enunciado

---
+++ Estructuras-Ciclicas/anidados/1.1.py
@@ -0,0 +1,2 @@
+#1. La empresa “Salome lujos” lo contrata para hacer un algoritmo que imprima el total de facturación de un día. Sabiendo que cada día se hacen 3 facturas y cada  factura tiene 2 artículos, la factura debe tener el nombre del artículo, precio y su cantidad. El descuento se basa en una balota (la factura tiene un solo descuento), si la balota es blanca es del 10%, si la balota es azul del 12%, si la balota es roja del 14%, si la balota es verde del 16%, si la balota es rosa es del 18%. y encuentre el valor de la semana sabiendo que la empresa solo abre 3 dias a la semana
+
