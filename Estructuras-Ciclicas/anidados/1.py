992cc49284197726b7775301dcd22e8f300b33a2
Juan Felipe Quintero Gutierrez <146882792+jfelipeq14@users.noreply.github.com>
2023-11-23 07:39:53 -0500

    ejercicio con explicacion del profesor

--- Estructuras-Ciclicas/anidados/1.py
+++ Estructuras-Ciclicas/anidados/1.py
@@ -1,57 +1,43 @@
 #1. La empresa “Salome lujos” lo contrata para hacer un algoritmo que imprima el total de facturación de un día. Sabiendo que cada día se hacen 3 facturas y cada  factura tiene 2 artículos, la factura debe tener el nombre del artículo, precio y su cantidad. El descuento se basa en una balota (la factura tiene un solo descuento), si la balota es blanca es del 10%, si la balota es azul del 12%, si la balota es roja del 14%, si la balota es verde del 16%, si la balota es rosa es del 18%.
 
-valorFactura1 = 0
-valorFactura2 = 0
-valorFactura3 = 0
-
+# variables (contadores, acumuladores)
+subTotal = 0
+valorFactura = 0
+valorDia = 0
+# for de las 3 facturas para pedir los producto
 for factura in range(1,4):
-    balota=input("Digite el color de la balota (blanca, azul, roja, verde, rosa):").lower()
-    
-    if balota == "blanca":
-        porcentajeDescuento = 0.10
-    elif balota == "azul":
-        porcentajeDescuento = 0.12
-    elif balota == "roja":
-        porcentajeDescuento = 0.14
-    elif balota == "verde":
-        porcentajeDescuento = 0.16
-    elif balota == "rosa":
-        porcentajeDescuento = 0.18
-
+    #for para los productos, que en este caso son 2
     for producto in range(1, 3):
         nombreArticulo=input(f"Digite el nombre del articulo {producto}: ")
         precioArticulo=int(input(f"Digite el precio de {nombreArticulo}: "))
         cantidadArticulo=int(input(f"Digite la cantidad de {nombreArticulo}: "))
+        #sacar el total de cada uno de los productos
         totalArticulo = precioArticulo*cantidadArticulo
+        #acumular los productos en el subtotal de la factura
+        subTotal += totalArticulo
 
-        if factura == 1:
-            valorFactura1+=totalArticulo
-        elif factura == 2:
-            valorFactura2 += totalArticulo
-        elif factura == 3:
-            valorFactura3 += totalArticulo
-    
-descuentoFactura1 = valorFactura1*porcentajeDescuento
-netoFactura1 = valorFactura1-descuentoFactura1
+    balota=input("Digite el color de la balota (blanca, azul, roja, verde, rosa): ").lower()
 
-descuentoFactura2 = valorFactura2*porcentajeDescuento
-netoFactura2 = valorFactura2-descuentoFactura2
+    # validar la balota para el descuento    
+    if balota == "blanca":
+        descuento = subTotal * 0.10
+    elif balota == "azul":
+        descuento = subTotal * 0.12
+    elif balota == "roja":
+        descuento = subTotal * 0.14
+    elif balota == "verde":
+        descuento = subTotal * 0.16
+    elif balota == "rosa":
+        descuento = subTotal * 0.18
 
-descuentoFactura3 = valorFactura3*porcentajeDescuento
-netoFactura3 = valorFactura3-descuentoFactura3
-
-totalDia = netoFactura1 + netoFactura2 + netoFactura2
-
-print(f"Valor de la factura 1: {valorFactura1}")
-print(f"Descuento de la factura 1: {descuentoFactura1}")
-print(f"Neto de la factura 1: {netoFactura1}")
-
-print(f"Valor de la factura 2: {valorFactura2}")
-print(f"Descuento de la factura 2: {descuentoFactura2}")
-print(f"Neto de la factura 2: {netoFactura2}")
-
-print(f"Valor de la factura 3: {valorFactura3}")
-print(f"Descuento de la factura 3: {descuentoFactura3}")
-print(f"Neto de la factura 3: {netoFactura3}")
-
-print(f"Total del dia: {totalDia}")
+    # sacar el valor total de la factura con su respectivo descuento
+    valorFactura = subTotal - descuento
+    #mostrar la factura
+    print(f"Valor de la factura: {valorFactura}")
+    #acumular el valor de las facturas
+    valorDia += valorFactura
+    #reinicializar las variablse para sacar otra facturas
+    valorFactura = 0
+    subTotal = 0
+#mostrar el valor total del dia
+print(f"Valor del dia: {valorDia}")
