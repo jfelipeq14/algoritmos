dcdf69a9072fe3790cda7dd31597df3f20dc2531
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-12-01 18:15:43 -0500

    ejercicio 2

--- entregableCiclos/2.py
+++ entregableCiclos/2.py
@@ -1,15 +1,36 @@
 # El supermercado “Todo hay” lo contrata a Ud. joven aprendiz para realizar un algoritmo
 # que calcule las ventas anuales, ventas semestrales y mensuales de la empresa. Cada mes
 # se generan 5 facturas y cada factura (cantidad * precio unitario) tiene 5 artículos.
-subtotal = 0
-factura = 0
-mes = 0
 
-for mes in range(1, 13):
-    for factura in range(1, 6):
-        for producto in range(1, 5):
-            precio = int(input(f"Digite el precio del producto {producto}: "))
-            cantidad = int(input(f"Digite la cantidad del producto {producto}: "))
-            valorProducto = precio * cantidad
-            subtotal += valorProducto
-            
+subtotal = 0
+facturas=0
+meses=0
+semestral=0
+anuales=0
+
+for semestre in range(0, 12, 6):
+    for mes in range(1, 3):
+        for factura in range(1, 3):
+            for producto in range(1, 3):
+                precio=int(input(f"Digite el precio del producto {producto}: "))
+                cantidad=int(input(f"Digite la cantidad del producto {producto}: "))
+                producto = cantidad * precio
+                subtotal += producto
+                facturas += subtotal
+                print(f"producto {producto}")
+                print(f"subtotal {subtotal}")
+                print(f"factura {facturas}")
+                print()
+                subtotal = 0
+            mes += facturas
+            print(f"mes {mes}")
+            print()
+            facturas = 0
+        semestre += mes
+        print(f"semestre {semestre}")
+        print()
+        mes = 0
+    anuales += semestre
+    print(f"anuales {anuales}")
+    print()
+    semestre = 0
--- entregableCiclos/4.py
+++ entregableCiclos/4.py
@@ -1,3 +1,4 @@
 # En su proyecto formativo se pide que calcule las comisiones de cada uno de los
 # vendedores de la empresa y el total de comisiones. Se conoce que la empresa tiene 6
 # vendedores y cada vendedor trae las facturas(5) del mes que vendió.
+
