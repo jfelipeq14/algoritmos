3c047b492a6c4fb0f0e0f022fe1e570597a2b403
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-12-01 17:20:43 -0500

    ejercicio 1 taller calificable

---
+++ entregableCiclos/1.py
@@ -0,0 +1,31 @@
+# La empresa “salome style” lo contrata para que muestre la facturación de un día sabiendo que en
+# el día hay 2 facturas y cada factura tiene 2 productos (cantidad y valor). Cada factura tiene un
+# descuento dependiendo del valor de dicha factura. Si la factura es menor o igual a 50.000 tiene un
+# descuento del 3%, si la factura es menor o igual a 100.000 tiene un descuento del 5% y si es mayor
+# el descuento es del 9%.
+
+dia = 0
+subtotal = 0
+for factura in range(1, 3):
+    for producto in range(1, 3):
+        precio = int(input(f"Digite el precio del producto {producto}: "))
+        cantidad = int(input(f"Digite la cantidad del producto {producto}: "))
+        valorProducto = precio*cantidad
+        subtotal += valorProducto
+    if subtotal <= 50000:
+        descuento = subtotal * 0.03
+    elif subtotal <= 100000:
+        descuento = subtotal * 0.05
+    elif subtotal > 100000:
+        descuento = subtotal * 0.09
+    neto = subtotal - descuento
+    dia += neto
+    print()
+    print(f"Valor factura: {subtotal}")
+    print(f"Valor descuento: {descuento}")
+    print(f"Valor neto: {neto}")
+    print(f"Valor dia: {dia}")
+    print()
+    subtotal = 0
+
+#Completed
---
+++ entregableCiclos/2.py
@@ -0,0 +1,14 @@
+# El supermercado “Todo hay” lo contrata a Ud. joven aprendiz para realizar un algoritmo
+# que calcule las ventas anuales, ventas semestrales y mensuales de la empresa. Cada mes
+# se generan 5 facturas y cada factura (cantidad * precio unitario) tiene 5 artículos.
+subtotal = 0
+factura = 0
+mes = 0
+
+for mes in range(1, 13):
+    for factura in range(1, 6):
+        for producto in range(1, 5):
+            precio = int(input(f"Digite el precio del producto {producto}: "))
+            cantidad = int(input(f"Digite la cantidad del producto {producto}: "))
+            valorProducto = precio * cantidad
+            subtotal += valorProducto
