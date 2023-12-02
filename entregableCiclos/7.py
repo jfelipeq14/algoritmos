463987c2e04705bb9122fade1e3574d7ab321ed7
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-12-01 20:26:01 -0500

    ejercicio 7

--- entregableCiclos/7.py
+++ entregableCiclos/7.py
@@ -5,3 +5,23 @@
 # 10,000,000 y MENOR A 50,000,000 4%
 # 50,000,000 y MENOR A 70,000,000 5%
 # 70,000,000 o MAS 6%
+
+ventas=0
+
+empleados = int(input("¿Cuantos empleados hay?: "))
+for empleado in range(empleados):
+    vendio = int(input(f"¿Cuantas ventas hizo el empleado {empleado+1}?: "))
+    for venta in range(vendio):
+        precioVenta=int(input(f"Cuanto vendio en la venta {venta+1}: "))
+        ventas += precioVenta
+
+    if ventas < 10000000:
+        comision = ventas * 0.03
+    elif ventas >= 10000000 and ventas < 50000000:
+        comision = ventas * 0.04
+    elif ventas >= 50000000 and ventas < 70000000:
+        comision = ventas * 0.05
+    elif ventas >= 70000000:
+        comision = ventas * 0.06
+
+    print(f"La comision del empleado {empleado+1}: {comision}")
--- entregableCiclos/8.py
+++ entregableCiclos/8.py
@@ -1,2 +1,3 @@
 # Diseñar un algoritmo que lea las ventas de 100 vendedores y que escriba la comisión anual
 # que le corresponda a cada vendedor. Suponga que vende 5 facturas por mes.
+
