cd24f030804a2bbce341b2a3342eaab681be5d95
jfelipeq14 <juan_fquintero@soy.sena.edu.co>
2023-12-09 14:30:02 -0500

    tablas de verdad con 'y' & 'o'

--- python/tablas-de-verdad.py
+++ python/tablas-de-verdad.py
@@ -1,8 +1,14 @@
 v = True
 f = False
-
+print()
 print(f"Tabla de verdad con '^' = 'y'")
-print(v and v)
-print(v and f)
-print(f and v)
-print(f and f)
+print(f"{v} y {v} : {v and v}")
+print(f"{v} y {f} : {v and f}")
+print(f"{f} y {v} : {f and v}")
+print(f"{f} y {f} : {f and f}")
+print()
+print(f"Tabla de verdad con 'v' = 'o'")
+print(f"{v} o {v} : {v or v}")
+print(f"{v} o {f} : {v or f}")
+print(f"{f} o {v} : {f or v}")
+print(f"{f} o {f} : {f or f}")
